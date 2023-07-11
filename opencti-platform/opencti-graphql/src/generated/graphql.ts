f97594a0c44fcc1fb9165553ef782b80197dacfe
sarahBocognano <sarah.bocognano@filigran.io>
2023-07-11 17:34:30 +0200

    [backend/frontend] Filter Image in MimeType and Add PictureManagementViewer WIP (#issue/2540)

--- opencti-platform/opencti-graphql/src/generated/graphql.ts
+++ opencti-platform/opencti-graphql/src/generated/graphql.ts
@@ -24609,6 +24609,7 @@ export type ThreatActorIndividual = BasicObject & StixCoreObject & StixDomainObj
   groupings?: Maybe<GroupingConnection>;
   id: Scalars['ID'];
   importFiles?: Maybe<FileConnection>;
+  importFilesMimeType?: Maybe<FileConnection>;
   is_inferred: Scalars['Boolean'];
   jobs?: Maybe<Array<Maybe<Work>>>;
   lang?: Maybe<Scalars['String']>;
@@ -24682,6 +24683,12 @@ export type ThreatActorIndividualGroupingsArgs = {
 
 export type ThreatActorIndividualImportFilesArgs = {
   first?: InputMaybe<Scalars['Int']>;
+};
+
+
+export type ThreatActorIndividualImportFilesMimeTypeArgs = {
+  first?: InputMaybe<Scalars['Int']>;
+  mimeType?: InputMaybe<Scalars['String']>;
 };
 
 
@@ -35471,6 +35478,7 @@ export type ThreatActorIndividualResolvers<ContextType = any, ParentType extends
   groupings?: Resolver<Maybe<ResolversTypes['GroupingConnection']>, ParentType, ContextType, Partial<ThreatActorIndividualGroupingsArgs>>;
   id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
   importFiles?: Resolver<Maybe<ResolversTypes['FileConnection']>, ParentType, ContextType, Partial<ThreatActorIndividualImportFilesArgs>>;
+  importFilesMimeType?: Resolver<Maybe<ResolversTypes['FileConnection']>, ParentType, ContextType, Partial<ThreatActorIndividualImportFilesMimeTypeArgs>>;
   is_inferred?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
   jobs?: Resolver<Maybe<Array<Maybe<ResolversTypes['Work']>>>, ParentType, ContextType, Partial<ThreatActorIndividualJobsArgs>>;
   lang?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
